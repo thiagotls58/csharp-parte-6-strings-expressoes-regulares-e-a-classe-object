2d6ed9bb50e721df54aa1bc963b56beb72d88a17
Thiago Luz <thiagotls58@gmail.com>
2020-12-29 08:55:09 -0300

    Aula 01

---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?argumentos";
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(7);
+            Console.WriteLine(argumentos);
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
