42c179680e149ebdd47cdc886fd10d3fbb27a643
Thiago Luz <thiagotls58@gmail.com>
2021-01-07 21:38:57 -0300

    Aula 04 e 05

--- ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ByteBank.Modelos;
+using System;
+using System.Text.RegularExpressions;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -6,6 +8,26 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            ContaCorrente c = new ContaCorrente(123, 789654);
+            Console.WriteLine(c);
+            Console.ReadLine();
+
+            string padrao = "Sala [A-G][-]?[0123456789]{2}";
+
+            Console.WriteLine(Regex.IsMatch("Sala G345", padrao));
+            Console.WriteLine(Regex.IsMatch("Sala J-001", padrao));
+            Console.WriteLine(Regex.IsMatch("Sala a004", padrao));
+            Console.WriteLine(Regex.IsMatch("Sala C004", padrao));
+            Console.WriteLine(Regex.IsMatch("Minha sala é a sala G34", padrao));
+            Console.WriteLine(Regex.IsMatch("Minha sala é a Sala G-34", padrao));
+            Console.ReadLine();
+
+            Console.WriteLine("Regex");
+            string texto = "Meu telefone é 9968328, entre em contato.";
+            Match resultado = Regex.Match(texto, "[0-9]{4,5}-?[0-9]{4}");
+            Console.WriteLine(resultado.Value);
+            Console.ReadLine();
+
             string url = "www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
             ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(url);
             Console.WriteLine(extrator.GetValor("moedaOrigem")); // real
@@ -13,6 +35,8 @@ namespace ByteBank.SistemaAgencia
             Console.WriteLine(extrator.GetValor("valor")); // 1500
 
             Console.ReadLine();
+
+
         }
     }
 
